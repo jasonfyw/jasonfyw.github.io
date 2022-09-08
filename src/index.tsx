da54feac60a9877e713d949e974e9476f1591854
Jason Wang <40612523+jasonfyw@users.noreply.github.com>
2022-09-08 01:10:18 -0400

    style: indentation

--- src/index.tsx
+++ src/index.tsx
@@ -9,10 +9,10 @@ if (!container) throw new Error('Failed to find the root element');
 const root = ReactDOM.createRoot(container)
 
 root.render(
-  <React.StrictMode>
-    <ColorModeScript />
-    <App />
-  </React.StrictMode>,
+    <React.StrictMode>
+        <ColorModeScript />
+        <App />
+    </React.StrictMode>,
 )
 
 
