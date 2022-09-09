e386e75dc4343a03e090f33d50c29fadce285a83
Jason Wang <40612523+jasonfyw@users.noreply.github.com>
2022-09-09 14:51:48 -0400

    add: basic footer component

--- src/App.tsx
+++ src/App.tsx
@@ -4,11 +4,14 @@ import theme from "./theme"
 import Nav from "./components/Nav"
 import Intro from "./components/Intro"
 import About from "./components/About"
+import Footer from "./components/Footer"
 
 export const App = () => (
     <ChakraProvider theme={theme}>
         <Nav />
         <Intro />
         <About />
+
+        <Footer />
     </ChakraProvider>
 )
