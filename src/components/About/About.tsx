7f1a08c8d8ceb0f647437126a66c96a5cc619f3e
Jason Wang <40612523+jasonfyw@users.noreply.github.com>
2022-09-10 01:02:16 -0400

    style: change paragraph styling

--- src/components/About/About.tsx
+++ src/components/About/About.tsx
@@ -21,15 +21,12 @@ const About: FC = () => {
                         src='https://raw.githubusercontent.com/jasonfyw/jasonfyw/main/media/headshot.png'
                         border={`3px solid ${useColorModeValue('#1E2428', '#ECEFF4')}`}
                     />
-                    <VStack spacing={4} p={4} pl={50}>
+                    <VStack spacing={4} p={4} pl={50} textAlign='justify'>
                         <Text>
                             Hi, I'm Jason and I'm an undergraduate student at the University of Toronto pursuing a Specialist in Computer Science and Major in Cognitive Science, graduating class of 2025.
                         </Text>
                         <Text>
-                            Having worked in a diverse range of professional environments from event planning to startups, I am experienced in full-stack web development - particularly with Javascript and Python. 
-                        </Text>
-                        <Text>
-                            In addition to SWE, I am also interested in machine learning, namely NLP and computational linguistics.
+                            Having worked in a diverse range of professional environments from event planning to startups, I am experienced in full-stack web development - particularly with Javascript and Python. In addition to SWE, I am also interested in machine learning, namely NLP and computational linguistics.
                         </Text>
                     </VStack>
                 </Flex>
