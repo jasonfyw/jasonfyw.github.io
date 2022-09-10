4f7ab61beed1fa9663300084d1237e59ba465c60
Jason Wang <40612523+jasonfyw@users.noreply.github.com>
2022-09-10 01:01:47 -0400

    style: bring navbar to topmost layer

--- src/components/Nav/Nav.tsx
+++ src/components/Nav/Nav.tsx
@@ -33,7 +33,7 @@ const Nav: FC = () => {
 
     return (
         <>
-            <Box bg={useColorModeValue('gray.100', 'gray.900')} px={4} position={'fixed'} top={0} width={'100vw'}>
+            <Box bg={useColorModeValue('gray.100', 'gray.900')} px={4} position={'fixed'} top={0} width={'100vw'} zIndex={100}>
                 <Flex h={16} alignItems={'center'} justifyContent={'space-between'}>
                     <IconButton
                         size={'md'}
