01a390a66aa230f1b33cf0d5a4e9431ac4f3cf2e
Jason Wang <40612523+jasonfyw@users.noreply.github.com>
2022-09-12 01:28:25 -0400

    add: resume component

--- src/App.tsx
+++ src/App.tsx
@@ -7,6 +7,7 @@ import About from "./components/About"
 import Footer from "./components/Footer"
 import Projects from "./components/Projects"
 import Skills from "./components/Skills"
+import Resume from "./components/Resume"
 
 export const App = () => (
     <ChakraProvider theme={theme}>
@@ -15,6 +16,7 @@ export const App = () => (
         <About />
         <Skills />
         <Projects />
+        <Resume />
 
         <Footer />
     </ChakraProvider>
---
+++ src/components/Resume/Resume.tsx
@@ -0,0 +1,25 @@
+import { FC } from 'react';
+import {
+    Container,
+    VStack,
+    Heading,
+    Box
+} from '@chakra-ui/react';
+import { Element } from 'react-scroll'
+
+const Resume: FC = () => {
+    return (
+        <Element name='resume'>
+            <Container maxW={'5xl'} p={6.75} mt={15}>
+                <VStack spacing={8}>
+                    <Heading as='h2'>Resume</Heading>
+                    <Container>
+                        <iframe src="https://drive.google.com/file/d/1_2r1Hb93Z_mSEhEck8InjcoeqUEh19Fg/preview" width="100%" height="480" allow="autoplay" title="Resume"></iframe>
+                    </Container>
+                </VStack>
+            </Container>
+        </Element>
+    );
+};
+
+export default Resume;
