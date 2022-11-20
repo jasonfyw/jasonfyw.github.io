22fcfd0e5d014e875cb7634dbfa97072fd1bb5f0
Jason Wang <40612523+jasonfyw@users.noreply.github.com>
2022-11-19 23:36:01 -0500

    add: links to project card image headers

--- src/components/Projects/Projects.tsx
+++ src/components/Projects/Projects.tsx
@@ -29,7 +29,8 @@ const projectData = [
         description: 'A web tool allowing queries analysing and visualising regional sentiment towards given topics on Twitter during a specific time frame. A custom-trained Naive Bayes model is used to perform sentiment analysis on aggregated data,.',
         links: [
             <LinkIconButton href="https://github.com/jasonfyw/twitter-geo-sentiment" icon={<FiGithub/>}/>
-        ]
+        ],
+        headerLink: 'https://github.com/jasonfyw/twitter-geo-sentiment'
     },
     {
         name: 'Orbital Simulation',
@@ -39,7 +40,8 @@ const projectData = [
         description: 'An implementation of Newtonian physics in PyGame using positional data from the JPL Horizons database to simulate and record data on the past and future movements of celestial bodies in our solar system and beyond.',
         links: [
             <LinkIconButton href="https://github.com/jasonfyw/orbital-sim" icon={<FiGithub/>}/>
-        ]
+        ],
+        headerLink: 'https://github.com/jasonfyw/orbital-sim'
     },
     {
         name: 'Fourier Series Animation',
@@ -50,7 +52,8 @@ const projectData = [
         links: [
             <LinkIconButton href="https://github.com/jasonfyw/fourier-series" icon={<FiGithub/>}/>,
             <LinkIconButton href="https://jasonfyw.com/fourier-series/" icon={<FiLink/>}/>
-        ]
+        ],
+        headerLink: 'https://jasonfyw.com/fourier-series/'
     },
     {
         name: 'PLISMUN',
@@ -61,7 +64,8 @@ const projectData = [
         links: [
             <LinkIconButton href="https://github.com/jasonfyw/twitter-geo-sentiment" icon={<FiGithub/>}/>,
             <LinkIconButton href="https://plismun.com" icon={<FiLink />}/>
-        ]
+        ],
+        headerLink: 'https://plismun.com'
     },
     {
         name: 'Blogging Application',
@@ -71,7 +75,8 @@ const projectData = [
         description: 'Our group project for CSC207 - a blogging website where users can post text content and follow other accounts. Designed and built according to CLEAN architecture and SOLID principles.',
         links: [
             <LinkIconButton href="https://github.com/jasonfyw/csc207-blogging-app" icon={<FiGithub/>}/>
-        ]
+        ],
+        headerLink: 'https://github.com/jasonfyw/csc207-blogging-app'
     }
 ]
 
@@ -94,6 +99,7 @@ const Projects: FC = () => {
                                     imageSrc={data.imageSrc}
                                     description={data.description}
                                     links={data.links}
+                                    headerLink={data.headerLink}
                                 />
                             </WrapItem>
                         ))
