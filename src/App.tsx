b6b41c5de89738d003bc66c12deb90aac48a232e
Jason Wang <40612523+jasonfyw@users.noreply.github.com>
2023-03-05 04:56:46 -0500

    remove: navbar from project page

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,6 @@
 import * as React from "react"
 import { ChakraProvider } from "@chakra-ui/react"
 import theme from "./theme"
-import Nav from "./components/Nav"
 import Footer from "./components/Footer"
 import { Route, Routes } from "react-router"
 import Main from "./pages/Main/Main"
@@ -11,7 +10,6 @@ import Project from "./pages/Project/Project"
 
 export const App = () => (
     <ChakraProvider theme={theme}>
-        <Nav />
         <Routes>
             <Route index path='/' element={<Main/>} />
             <Route path='/projects' element={<Projects/>} />
--- src/pages/Main/Main.tsx
+++ src/pages/Main/Main.tsx
@@ -5,10 +5,12 @@ import Projects from "../../components/Projects"
 import Skills from "../../components/Skills"
 import Resume from "../../components/Resume"
 import Contact from "../../components/Contact"
+import Nav from '../../components/Nav';
 
 const Main = () => {
     return (
         <>
+            <Nav />
             <Intro />
             <About />
             <Skills />
