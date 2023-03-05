a97f78a623f59d8a863608d06fcce6e486b031b6
Jason Wang <40612523+jasonfyw@users.noreply.github.com>
2023-03-04 22:44:16 -0500

    fix: changed error handling

--- src/pages/Project/ProjectContent.tsx
+++ src/pages/Project/ProjectContent.tsx
@@ -35,7 +35,7 @@ const ProjectContent = (props: ProjectContentProps) => {
                     .then(res => setMarkdown(res))
                     .catch(err => console.log(err));
             })
-            .catch(err => console.log(err));
+            .catch(err => {});
     })
 
     return (
