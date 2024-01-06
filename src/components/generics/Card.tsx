b146d98868db7e319c1ed858c4c3424cc6b46002
Jason Wang <40612523+jasonfyw@users.noreply.github.com>
2024-01-06 03:50:07 +0100

    change: card bg opacities

--- src/components/generics/Card.tsx
+++ src/components/generics/Card.tsx
@@ -12,11 +12,11 @@ interface CardProps extends BoxProps {}
 const Card: React.FC<PropsWithChildren<CardProps>> = ({ children, ...restProps }) => {
     return (
         <Box
-            bg={useColorModeValue('rgba(255, 255, 255, 0.5)', 'rgba(23, 25, 35, 0.6)')}
+            bg={useColorModeValue('rgba(255, 255, 255, 0.33)', 'rgba(23, 25, 35, 0.33)')}
             boxShadow={'xl'}
             rounded={'xl'}
             p={6}
-            backdropFilter={useColorModeValue('blur(1px)', 'blur(2.5px)')}
+            backdropFilter={useColorModeValue('blur(1.5px)', 'blur(2.5px)')}
             { ...restProps }
         >
             { children }
