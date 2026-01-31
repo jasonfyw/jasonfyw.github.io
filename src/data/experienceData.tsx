2889a57f4a1c9046834714013c8ad1877cb313aa
Jason Wang <jasonwang0610@gmail.com>
2026-01-30 22:28:39 -0500

    add: dg experience (#120)

--- src/data/experienceData.tsx
+++ src/data/experienceData.tsx
@@ -1,11 +1,20 @@
 import { ExperienceData } from './types'
 import nerveXLogo from '../media/nervex_logo.jpeg'
 import blueprintLogo from '../media/blueprint_logo.jpeg'
-import helloFairLogo from '../media/hellofair_logo.jpeg'
 import mieLogo from '../media/uoftmie_logo.jpeg'
 import theScoreLogo from '../media/thescore_logo.jpeg'
+import deepGenomicsLogo from '../media/dg_logo.jpeg'
 
 export const experienceData: ExperienceData[] = [
+    {
+        title: 'Software Developer',
+        company: 'Deep Genomics',
+        location: 'Toronto, ON',
+        dates: 'January 2026 - Present',
+        description:
+            'Building internal data tools for scientists and ML researchers to accelerate genome research and analysis.',
+        logoSrc: deepGenomicsLogo
+    },
     {
         title: 'Software Developer',
         company: 'theScore',
@@ -41,24 +50,5 @@ export const experienceData: ExperienceData[] = [
         description:
             'Enhanced API efficiency, developed a medical charting library, and streamlined data pipelines using modern technologies like .NET Core, ReactJS, TypeScript, Docker, and AWS Lambda.',
         logoSrc: nerveXLogo,
-    },
-    {
-        title: 'Web Developer',
-        company: 'UTMIST',
-        location: 'Toronto, ON',
-        dates: 'October 2022 - June 2023',
-        description:
-            'Designed and implemented the website for the inaugural edition of Hack the Mist, an applied machine learning MLH-partnered hackathon, by using ReactJS, Typescript, along with ChakraUI.',
-        logoSrc: 'https://avatars.githubusercontent.com/u/42724991?s=200&v=4',
-    },
-    {
-        title: 'Web Developer',
-        company: 'Fair',
-        location: 'Oxford, UK (Remote)',
-        dates: 'May 2021 - May 2022',
-        description:
-            'Developed a prototype paper-trading web application using NextJS and Golang with live price information and trading functionality with mock accounts stored in MongoDB.',
-        logoSrc:
-            helloFairLogo,
     },
 ]
